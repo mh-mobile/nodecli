9215cda90d6b9d34e3d24e54d9e707758ecd764e
mbyk <marubayashi@gmail.com>
2020-05-28 13:53:21 +0900

    cliでmarkdownをhtmlに変換

---
+++ main.js
@@ -0,0 +1,24 @@
+const program = require("commander");
+const fs = require("fs");
+const marked = require("marked");
+
+program.option("--gfm", "GFMを有効にする");
+program.parse(process.argv);
+const filePath = program.args[0];
+
+const cliOptions = {
+  gfm: false,
+  ...program.opts(),
+};
+
+fs.readFile(filePath, { encoding: "utf8" }, (error, file) => {
+  if (error) {
+    console.error(error.message);
+    process.exit(1);
+    return;
+  }
+  const html = marked(file, {
+    gfm: cliOptions.gfm,
+  });
+  console.log(html);
+});
